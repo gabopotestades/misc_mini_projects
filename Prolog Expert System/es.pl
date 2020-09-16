ed43efc2d72c0c1ea4ed63478219546b64800c97
gabopotestades <grpotestades@gmail.com>
2020-09-16 23:44:34 +0800

    Added chief complaint.

--- Prolog Expert System/es.pl
+++ Prolog Expert System/es.pl
@@ -1,6 +1,9 @@
 :- dynamic symptom/1, not_symptom/1.
 
 diagnose :-
+    write('Chief complaint: '), nl, 
+    chief_complaint(Comp, _),
+    chief_complaint_query(Comp),
     write('Does patient have the following symptoms: '), nl, 
     possible_disease(Dis),
     disease(Dis, Desc),
@@ -9,6 +12,17 @@ diagnose :-
 
 check_symptom(Symp) :-
     (symptom(Symp) -> true ; (not_symptom(Symp) -> fail ; query(Symp))). 
+
+chief_complaint_query(Symp) :-
+    chief_complaint(Symp, Desc),
+    write(Desc),
+    write('? '),
+    read(Ans), nl,
+    chief_complaint_add_symptom(Symp, Ans).
+
+chief_complaint_add_symptom(Symp, Ans) :-
+    (Ans == y ->  (assert(symptom(Symp)) , fail) ; 
+    assert(not_symptom(Symp)), fail) ; (Symp == breathing -> true).
 
 query(Symp) :-
     possible_symptoms(Symp, Desc),
@@ -20,6 +34,13 @@ query(Symp) :-
 add_symptom(Symp, Ans) :-
     (Ans == y ->  assert(symptom(Symp)) ; 
     assert(not_symptom(Symp)), fail).
+
+chief_complaint(fatigue, 'Fatigue').
+chief_complaint(fever, 'Fever').
+chief_complaint(headache, 'Headache').
+chief_complaint(chest_pain, 'Chest pains').
+chief_complaint(coughing, 'Coughing').
+chief_complaint(breathing, 'Difficulty breathing').
 
 possible_disease(acute_respiratory_infection).
 possible_disease(pneumonia).
@@ -33,17 +54,19 @@ possible_disease(sleep_apnea).
 possible_disease(malaria).
 possible_disease(none).
 
-possible_symptoms(fever, 'Fever').
+/*
 possible_symptoms(fatigue, 'Fatigue').
+possible_symptoms(fever, 'Fever').
 possible_symptoms(headache, 'Headache').
-possible_symptoms(swallow, 'Having a hard time swallowing').
-possible_symptoms(wheezing, 'Wheezing when breathing').
+possible_symptoms(chest_pain, 'Chest pains').
 possible_symptoms(coughing, 'Coughing').
 possible_symptoms(breathing, 'Difficulty breathing').
+*/
+possible_symptoms(swallow, 'Having a hard time swallowing').
+possible_symptoms(wheezing, 'Wheezing when breathing').
 possible_symptoms(heartbeat, 'Rapid Heartbeat').
 possible_symptoms(shivering, 'Shivering').
 possible_symptoms(appetite, 'Lost of appetite').
-possible_symptoms(chest_pain, 'Chest pains').
 possible_symptoms(mucus, 'Production of mucus').
 possible_symptoms(throat, 'Sore throat').
 possible_symptoms(runny_nose, 'Runny nose').
@@ -62,7 +85,7 @@ possible_symptoms(fainting, 'Fainting').
 possible_symptoms(lightheadedness, 'Lightheadedness').
 possible_symptoms(daytime, 'Excessive daytime sleepiness').
 possible_symptoms(snoring, 'Frequent loud snoring').
-possible_symptoms(reduced, 'Reduced or absent breathing, known as apnea events').
+possible_symptoms(reduced, 'Reduced or absent breathing').
 possible_symptoms(vomiting, 'Vomiting').
 possible_symptoms(nausea, 'Nausea').
 possible_symptoms(chills, 'Chills').
@@ -80,7 +103,7 @@ disease(tuberculosis, 'Tuberculosis') :-
 disease(uti, 'Urinary Tract Infection') :-
                 uti.
 disease(diabetes, 'Diabetes') :-
-                diabetes, !.
+                diabetes.
 disease(cardiomyopathy, 'Cardiomyopathy') :-
                 cardiomyopathy.
 disease(sleep_apnea, 'Obstructive Sleep Apnea') :-
@@ -91,75 +114,54 @@ disease(malaria, 'Malaria') :-
 disease(none, 'None').
 
 acute_respiratory_infection :-
-    check_symptom(fever), 
-    check_symptom(fatigue),
-    check_symptom(headache),
-    check_symptom(swallow),
-    check_symptom(wheezing).
+    (check_symptom(fatigue) ; check_symptom(fever) ; check_symptom(headache)),
+    check_symptom(wheezing),
+    check_symptom(swallow).
 
 pneumonia :-
-    check_symptom(fever), 
-    check_symptom(breathing),
-    check_symptom(heartbeat),
-    check_symptom(shivering),
+    (check_symptom(fever) ; check_symptom(chest_pain) ; check_symptom(breathing)),
     check_symptom(appetite),
-    check_symptom(chest_pain).
+    (check_symptom(shivering) ; check_symptom(heartbeat)).
 
 bronchitis :-
-    check_symptom(fever), 
-    check_symptom(coughing),
-    check_symptom(fatigue),
-    check_symptom(breathing),
-    check_symptom(mucus),
-    (check_symptom(wheezing) ; check_symptom(chest_pain)).
+    (check_symptom(fever) ; check_symptom(fatigue)),
+    (check_symptom(coughing) ; check_symptom(breathing) ; check_symptom(wheezing)),
+    (check_symptom(chest_pain) ; check_symptom(mucus)).
 
 influenza :-
-    check_symptom(headache), 
-    check_symptom(coughing),
-    check_symptom(fatigue),
-    check_symptom(throat),
-    check_symptom(runny_nose),
-    check_symptom(muscle_pain).
+    (check_symptom(fatigue) ; check_symptom(headache)), 
+    (check_symptom(coughing) ; check_symptom(throat)),
+    (check_symptom(runny_nose) ; check_symptom(muscle_pain)).
 
 uti :-
     check_symptom(fever), 
-    check_symptom(back_pain),
-    check_symptom(lower_abdominal),
+    (check_symptom(back_pain) ; check_symptom(lower_abdominal)),
     (check_symptom(burning) ; check_symptom(frequent) ; check_symptom(difficulty_urinating)).
 
 tuberculosis :-
-    check_symptom(fever),
-    check_symptom(coughing),
-    check_symptom(fatigue),
-    check_symptom(chest_pain),
-    check_symptom(night_sweats),
-    (check_symptom(appetite) ; check_symptom(weight_loss)).
+    (check_symptom(fever) ; check_symptom(fatigue)),
+    (check_symptom(coughing) ; check_symptom(chest_pain)),
+    (check_symptom(appetite) ; check_symptom(weight_loss)),
+    check_symptom(night_sweats).
 
 diabetes :-
     check_symptom(fatigue),
-    check_symptom(frequent),
-    check_symptom(weight_loss),
-    check_symptom(blurred_vision),
-    (check_symptom(excessive_thirst) ; check_symptom(excessive_hunger)).
+    (check_symptom(frequent) ; check_symptom(weight_loss)),
+    (check_symptom(excessive_thirst) ; check_symptom(excessive_hunger)),
+    check_symptom(blurred_vision).
 
 cardiomyopathy :-
-    check_symptom(breathing),
     check_symptom(fatigue),
-    check_symptom(chest_pain),
-    check_symptom(fainting),
-    check_symptom(lightheadedness).
+    (check_symptom(breathing) ; check_symptom(chest_pain)),
+    (check_symptom(fainting) ; check_symptom(lightheadedness)).
 
 sleep_apnea :-
-    check_symptom(headache),
-    check_symptom(throat),
-    check_symptom(snoring),
-    check_symptom(reduced),
-    (check_symptom(fatigue) ; check_symptom(daytime)).
+    (check_symptom(fatigue) ; check_symptom(daytime)),
+    (check_symptom(headache) ; check_symptom(throat)),
+    (check_symptom(snoring) ; check_symptom(reduced)).
 
 malaria :-
-    check_symptom(fever),
-    check_symptom(headache),
-    check_symptom(fatigue),
+    (check_symptom(fatigue) ; check_symptom(fever) ; check_symptom(headache)),
     (check_symptom(chills) ; check_symptom(night_sweats)),
     (check_symptom(vomiting) ; check_symptom(nausea)).
 
